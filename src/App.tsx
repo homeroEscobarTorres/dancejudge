42944379f649d6572a0e9d3e667378a0ac6f4288
Homero Escobar Torres <homero.escobar@outlook.it>
2024-03-21 16:21:07 +0100

    [FEAT] dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,106 @@
-import React from 'react';
-import { Grommet, Header, Page, PageContent, PageHeader, Text  } from 'grommet'
+import React, { useState, useContext } from "react";
+import { Moon, Sun } from "grommet-icons";
+import {
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Grid,
+  grommet,
+  Grommet,
+  Header,
+  Heading,
+  Page,
+  PageContent,
+  PageHeader,
+  Paragraph,
+  ResponsiveContext,
+  Text,
+} from "grommet";
+import { deepMerge } from "grommet/utils";
 
-const theme = {
+const theme = deepMerge(grommet, {
   global: {
     colors: {
-      brand: '#228BE6'
+      brand: "#228BE6",
     },
     font: {
       family: "Roboto",
-      size: "18px",
+      size: "14px",
       height: "20px",
     },
   },
-};
+});
 
 const AppBar = (props: any) => (
   <Header
     background="brand"
     pad={{ left: "medium", right: "small", vertical: "small" }}
-    elevation="medium"
+    elevation="small"
     {...props}
   />
 );
 
-const App = () => {
+const CardTemplate = ({ title }: any) => {
+  const size = useContext(ResponsiveContext);
   return (
-    <Grommet theme={theme} full>
+    <Card height="small" width="small">
+      <CardHeader pad="small">
+        <Heading level={2} margin="none">
+          {title}
+        </Heading>
+      </CardHeader>
+      <CardBody pad="small">
+        <Paragraph maxLines={size === "small" ? 3 : undefined}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+          porttitor non nulla ac vehicula. Aliquam erat volutpat. Mauris auctor
+          faucibus est at mattis. Aliquam a enim ac nisi aliquam consectetur et
+          ac velit. Mauris ut imperdiet libero.
+        </Paragraph>
+      </CardBody>
+    </Card>
+  );
+};
+
+const App = () => {
+  const [dark, setDark] = useState(false);
+
+  return (
+    <Grommet theme={theme} full themeMode={dark ? "dark" : "light"}>
       <Page>
-      <AppBar>
-       <Text size="large">Dance Judge</Text>
-     </AppBar>
-     <PageContent>
-     <PageHeader title="Welcome to Dance Judge (Bachata Sensual Edition)!" />
-   </PageContent>
+        <AppBar>
+          <Text size="large">My App</Text>
+          <Button
+            a11yTitle={dark ? "Switch to Light Mode" : "Switch to Dark Mode"}
+            icon={dark ? <Moon /> : <Sun />}
+            onClick={() => setDark(!dark)}
+            tip={{
+              content: (
+                <Box
+                  pad="small"
+                  round="small"
+                  background={dark ? "dark-1" : "light-3"}
+                >
+                  {dark ? "Switch to Light Mode" : "Switch to Dark Mode"}
+                </Box>
+              ),
+              plain: true,
+            }}
+          />
+        </AppBar>
+        <PageContent>
+          <PageHeader title="Welcome to Grommet!" />
+          <Grid columns="medium" gap="large" pad={{ bottom: "large" }}>
+            <CardTemplate title={"Card 1"} />
+            <CardTemplate title={"Card 2"} />
+            <CardTemplate title={"Card 3"} />
+          </Grid>
+        </PageContent>
       </Page>
     </Grommet>
   );
-}
+};
 
 export default App;
