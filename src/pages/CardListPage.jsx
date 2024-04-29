5a32833a3dbe338c798fd97553a51ae052cf8d8c
Homero Escobar Torres <homero.escobar@outlook.it>
2024-04-29 15:44:28 +0200

    [FIX]

--- src/pages/CardListPage.jsx
+++ src/pages/CardListPage.jsx
@@ -468,7 +468,7 @@ const CardListPage = ({onUpdateLoading, onUpdateBatteryList, batteryList, primar
   }, [batteryList.length === 0, setValueCards]);
 
   useEffect(() => {
-    setTitleCard(`You're judging battery number ${Object.keys(batterie)[0]}`);
+    setTitleCard(`You're judging battery number ${Object.keys(batteryList)[0]}`);
   }, [batteryList.length > 0]);
 
   const sendDataToParent = (loading) => {
